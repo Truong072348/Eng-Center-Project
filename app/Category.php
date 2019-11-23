c50cb5e0157628bf90b3b4de7a03eb378a89300a
Truong072348 <47754112+Truong072348@users.noreply.github.com>
2019-11-23 08:14:04 +0700

    fix

--- app/Category.php
+++ app/Category.php
@@ -6,16 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
     protected $table = "category";
     public $timestamps = false;
 
     public function type(){
-    	return $this->hasMany('App\CategoryType','id_category','id');
+    return $this->hasMany('App\CategoryType','id_category','id');
     }
-
     public function course(){
-        return $this->hasManyThrough('App\Course', 'App\CategoryType', 'id_category', 'id_ctype', 'id', 'id');
+    return $this->hasManyThrough('App\Course', 'App\CategoryType', 'id_category', 'id_ctype', 'id', 'id');
     }
 
 }
